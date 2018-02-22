e37d2071d17514accc3e968f24bd9e671359c80b
Jessica Hsiao <hsiaoj98@gmail.com>
2018-02-22 12:50:51 -0800

    lmao

--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -8,17 +8,43 @@ class Person
 
   def nickname
     # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    @name.to_s[0, 4]
   end
 
   def birth_year
     # YOUR IMPLEMENTATION HERE
-    1990
+    current_year = Time.now.year.to_i
+    current_year - @age.to_i - 1
   end
 
   def introduction
     # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    @name.to_s + ', ' + @age.to_s
   end
+
+  def profile_picture
+    image_tag("jeju_profile.jpg")
+  end
+
+  def myname
+    'Jessica'
+  end 
+
+  def where
+    'San Jose'
+  end
+
+  def school_year
+    'sophomore'
+  end
+
+  def fun_fact
+    "I like photography and taking Buzzfeed quizzes to find out when I'll no longer be single"
+  end
+
+  def excited
+    "I'm excited to build web apps!!!"
+  end
+
 
 end
--- app/controllers/concerns/stringify.rb
+++ app/controllers/concerns/stringify.rb
@@ -8,5 +8,10 @@ class Stringify
 
   def what_am_i
   	# YOUR IMPLEMENTATION HERE
+  	if @name.blank? == true || @adjective.blank? == true
+  		return "You are nothing!"
+  	else
+  		return @name.to_s + " is so " + @adjective.to_s
+    end
   end
 end
